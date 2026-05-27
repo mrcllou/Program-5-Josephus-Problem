81647484e42195fd6ffa4676d0c8118dc0ec7f11
OwenTjandra <owen.dunateo@gmail.com>
2026-05-27 11:57:59 -0700

    return true if 1 person left in boolean isOver() method

--- JosephusSim.java
+++ JosephusSim.java
@@ -1,4 +1,3 @@
-//Modify 
 import java.util.*;
 import java.io.*;
 
@@ -12,31 +11,30 @@ public class JosephusSim {
       try {
          // load names from the file in order, generating a singly linked list of PersonNodes
          Scanner file = new Scanner(new File("people.txt"));
-         while(file.hasNextLine()) {
-            String name = file.nextLine().trim();  //to remove extra spaces (from the last assigment) 
-               if(!name.isEmpty()) {
-                  add(name);
-                  size++;
-             }
+         while (file.hasNextLine()){
+            String name = file.nextLine().trim();
+            if(!name.isEmpty()){
+               add(name);
+               size++;
+            }
          }
-             
-         // make the ring circular by attaching last node's next to front
-         PersonNode last = circle; 
-         while(last.next != null) {
-            last = last.next; 
-         }
-         last.next = circle; 
          
+         // make the ring circular by attaching last node's next to front
+         PersonNode last = circle;
+         while(last.next != null) {
+            last = last.next;
+         }
+         last.next = circle;
          // remember the last node as the one in front of the next to get eliminated
-         track = last; 
+         track = last;
          
          // generate, print, and save the random elimination count
-         Random rand = new Random(); 
+         Random rand = new Random();
          int maxRange = Math.max(1, size/2);
          eliminationCount = rand.nextInt(maxRange) + 1;
          
-         System.out.println("=== Elimination count is " + eliminationCount + " ==="); 
-
+         System.out.println("=== Elimination count is " + eliminationCount + " ===");
+         
       } catch(FileNotFoundException e) {
          System.out.println("Something went wrong with " + fileName);
       }
@@ -48,26 +46,24 @@ public class JosephusSim {
    
    public void eliminate() {
       // count to the elimination count
-      for(int i = 0; i < eliminationCount; i++){
-         track = track.next; 
-         
+      for (int i = 0; i < eliminationCount; i++) {
+         track = track.next;
       }
-       
+      
       // print who will be eliminated
-      PersonNode victim = track.next; 
-      System.out.println(track.next + "eliminated !");       
+      PersonNode victim = track.next;
+      System.out.println(track.next + "eliminated!");
+      
       // eliminate the person and update "front" of the circle and size
+      track.next = victim.next;
+      size--;
       
-      track.next = victim.next; 
-      size--; 
-      
-      circle = track.next; 
-
+      circle = track.next;
    }
    
    public boolean isOver() {
       // check if there's only one person left in the circle
-      return false;
+      return size == 1;
    }
    
    public String toString() {
